2cba6f3d3271ec2bb237fd5ab0431230e3ccf723
RENE <RENE@MRI-BANGHO-I7>
2020-06-10 20:02:29 -0300

    creacion linea

---
+++ TPFINALLPOO-GRUPO03/ClasesBase/Linea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesBase
+{
+    class Linea
+    {
+        private int li_Id;
+        private string li_Descripcion;
+    }
+}
