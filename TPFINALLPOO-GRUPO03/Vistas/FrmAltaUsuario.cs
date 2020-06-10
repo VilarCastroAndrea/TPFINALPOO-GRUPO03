df7310d5b6c04dc0ef8a3f9bcbde569107b8660a
RENE <RENE@MRI-BANGHO-I7>
2020-06-10 15:59:48 -0300

    vALIDACION DE CAMPOS VACIOS Y CORRECTOS CON SETpROVIDER

--- TPFINALLPOO-GRUPO03/Vistas/FrmAltaUsuario.cs
+++ TPFINALLPOO-GRUPO03/Vistas/FrmAltaUsuario.cs
@@ -106,7 +106,9 @@ namespace Vistas
 
         private void txtNombreApellidoUsuario_KeyPress_1(object sender, KeyPressEventArgs e)
         {
+            ErrorProvider errorProvider = new ErrorProvider();
             Validar.soloLetra(e);
+            errorProvider.SetError(txtNombreApellidoUsuario, "ingrese datos validos");
         }
     }
 }
